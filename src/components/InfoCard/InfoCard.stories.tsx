82f13085ffa8f711e8ec3e4701baa23ff61f6c96
Joy Yu <joy.yu@geminidata.com>
2025-10-31 15:38:13 +0800

    feat(component): InfoCard

---
+++ src/components/InfoCard/InfoCard.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import InfoCard from "./InfoCard";
+
+const meta: Meta<typeof InfoCard> = {
+  title: "Components/InfoCard",
+  component: InfoCard,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          backgroundColor: "var(--color-neutral-900)",
+          height: "100vh",
+          padding: "10px",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof InfoCard>;
+
+export const Default: Story = {
+  args: {
+    infoName: "percipitation",
+    value: "0 mm",
+  },
+};
+
+Default.storyName = "InfoCard";
---
+++ src/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,16 @@
+import css from "./InfoCard.module.css";
+
+interface Props {
+  infoName: string;
+  value: string;
+}
+
+export default function InfoCard(props: Props) {
+  const { infoName, value } = props;
+  return (
+    <div className={css.InfoCard}>
+      <span className={css.infoName}>{infoName}</span>
+      <span className={css.infoValue}>{value}</span>
+    </div>
+  );
+}
